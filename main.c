0fe4268f549fd1ff9fc2ac3aaea49aa844944545
wannabil <nabil@wannabil.com>
2025-06-11 16:50:10 +0800

    show prompts, read inputs and exits on exit

---
+++ main.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[]) {
+	char *line = NULL;
+	size_t len = 0;
+	ssize_t read;
+	
+	while(1) /* infinite loop of "$ " */
+	{
+		printf("$ ");
+		fflush(stdout); /* clear out buffer */
+		read = getline(&line, &len, stdin); /* read input from user */
+
+		line[strlen(line) - 1] = '\0'; /* clean line to trim newline */
+		/* type 'exit' and [ENTER] to exit */
+		if (strcmp(line, "exit") == 0)
+		{
+			exit(EXIT_SUCCESS);
+		}
+	
+		else {
+			printf("$ you stuck here :(, CTRL-C to escape\n");
+		}
+	}
+}
