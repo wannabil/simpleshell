c37720ff54a7f67c36db91aba5a1574824c6d619
wannabil <wannabil.xyz@gmail.com>
2025-11-05 20:37:08 +0800

    added pwd

--- main.c
+++ main.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <unistd.h>  /* for fork(), execvp() */
+#include <sys/wait.h>  /* for wait() */
 
 int main(int argc, char *argv[]) {
 	char *line = NULL;
@@ -27,11 +29,30 @@ int main(int argc, char *argv[]) {
 				count += 1;
 				token = strtok(NULL, " "); // going to next token
 			}
-				printf("command : %s\n", tokens[0]);
-				for (int i = 1; i < count; i++)
-				{
-					printf("arg%d: %s\n", i, tokens[i]);
-				}
+			tokens[count] = NULL;  /* execvp requires NULL-terminated array */
+		
+
+			/* built-in pwd */ 	
+			if (strcmp(tokens[0], "pwd") == 0) {
+				char cwd[1024];
+				if (getcwd(cwd, sizeof(cwd)) != NULL)
+					printf("[simpleshell]%s\n", cwd);
+				else
+					perror("getcwd() error pls check");
+				continue; // skip execvp
 			}
+
+			pid_t pid = fork();  /* create child process */
+			if (pid == 0) {
+				/* Child process: execute the command */
+				execvp(tokens[0], tokens);
+				/* If execvp returns, there was an error */
+				printf("Command not found\n");
+				exit(EXIT_FAILURE);
+			} else {
+				/* Parent process: wait for child to finish */
+				wait(NULL);
+			}
+		}
 		}
 }
